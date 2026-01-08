0c706fe6734c8ba1452d4631af7212d9acf889a5
Marvin-Baafi-Owusu-hub <owusumarvin77.com>
2026-01-08 16:09:10 +0000

    added ml-4 on footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -94,7 +94,7 @@ const Footer = () => {
           </div>
 
           <div className="md:col-span-1"> 
-            <h4 className="text-lg font-semibold mb-5 text-white">Contact Us</h4>
+            <h4 className="text-lg font-semibold mb-5 ml-4 text-white">Contact Us</h4>
             <ul className="space-y-3 text-gray-400 text-sm">
               <li className="flex items-start">
                 <Mail className="w-5 h-5 mr-3 text-blue-400 shrink-0 mt-1" />
